bf9a89f5c346da90e849d29ed27424bc23f6bb80
bugyalu <774933704@qq.com>
2023-02-25 22:55:53 +0800

    chore: add env.prod

--- src/env.prod.ts
+++ src/env.prod.ts
@@ -1,12 +1,16 @@
-export const secretID = 'xxxx'
-export const secretKey = 'xxxx'
-export const mysqlSecret = 'root'
+export const secretID = process.env.SECRET_ID as string
+export const secretKey = process.env.SECRET_KEY as string
 
-export const uploadAccessKey = 'xxxx'
-export const uploadSecretKey = 'xxxx'
+export const mysqlUser = process.env.MYSQL_USER as string
+export const mysqlSecret = process.env.MYSQL_PASSWORD as string
+export const mysqlPort = Number(process.env.MYSQL_PORT) as number
+export const mysqlHost = process.env.MYSQL_HOST as string
+
+export const uploadAccessKey = process.env.UPLOAD_ACCESS_KEY as string
+export const uploadSecretKey = process.env.UPLOAD_SECRET_KEY as string
 export const uploadOptions = {
-  scope: 'xxxx',
+  scope: process.env.UPLOAD_SCOPE as string,
   isPrefixalScope: 1,
-  persistentOps: 'xxx',
-  persistentPipeline: 'xxx'
+  persistentOps: process.env.UPLOAD_PERSISTENT_OPS as string,
+  persistentPipeline: process.env.UPLOAD_PERSISTENT_PIPELINE as string
 }
