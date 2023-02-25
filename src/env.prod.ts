c7154977be5d52cf4db0d7c1ffe7ff9f63eee65a
bugyalu <774933704@qq.com>
2023-02-25 17:50:02 +0800

    style: update env.prod

--- src/env.prod.ts
+++ src/env.prod.ts
@@ -2,11 +2,11 @@ export const secretID = 'xxxx'
 export const secretKey = 'xxxx'
 export const mysqlSecret = 'root'
 
-export const uploadAccessKey='xxxx'
-export const uploadSecretKey='xxxx'
-export const uploadOptions={
-    scope: 'xxxx',
-    isPrefixalScope: 1,
-    persistentOps:'xxx',
-    persistentPipeline:'xxx',
+export const uploadAccessKey = 'xxxx'
+export const uploadSecretKey = 'xxxx'
+export const uploadOptions = {
+  scope: 'xxxx',
+  isPrefixalScope: 1,
+  persistentOps: 'xxx',
+  persistentPipeline: 'xxx'
 }
